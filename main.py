1d35e73eaa674bb1f25af237888aa22e0feba529
Sameer-Jha <sameer.btech.cs17@iiitranchi.ac.in>
2020-07-03 09:32:21 +0530

    Added features and git ignore update

--- main.py
+++ main.py
@@ -2,7 +2,17 @@
 import wikipedia as wiki
 from os import system
 from os.path import exists
+from string import ascii_lowercase
+from random import choice
 import argparse
+
+
+def rand_string_gen(len):
+    sel_space = ascii_lowercase
+    name = ''
+    for i in range(len):
+        name = name+choice(sel_space)
+    return name
 
 
 def search(term):
@@ -11,55 +21,87 @@ def search(term):
     else:
         system(f'echo "{wiki.summary(term)}"')
 
-def full_search(term):
-     if exists("/usr/bin/less"):
-        system(f'echo "{wiki.page(term).content}" | less')
-     else:
-        system(f'echo "{wiki.page(term).content}"')
-    
 
-def term_search(search_term, full_flag = False):
+def full_search(term):
+    data_file = temp_reader(term)
+    if exists("/usr/bin/less"):
+        system(f'less {data_file}')
+    else:
+        system(f'cat {data_file}')
+    system(f'rm -rf {data_file}')
+
+
+def temp_reader(search_data):
+    file = open(f'/tmp/{rand_string_gen(5)}.txt','w+')
+    file.write(wiki.page(search_data).content)
+    name = file.name
+    file.close()
+    return name
+
+
+
+def term_search(search_term, full_flag=False):
     searcher = wiki.search(search_term)
     s_len = len(searcher)
     if full_flag == False:
-        if(s_len>1):
+        if s_len > 1:
             for i in range(s_len):
-                print(f'{i+1}: {searcher[i]}')
+                print(f"{i+1}: {searcher[i]}")
 
-            f_term = int(input(f'select between 1 & {s_len}: ')) - 1
-            search(searcher[f_term])
+            print('0: Exit Search')
+            f_term = int(input(f"select between 1 & {s_len}: ")) - 1
+            if f_term+1>=1 and f_term+1<=s_len:
+                search(searcher[f_term])
+            else:
+                print('Exitting search')
 
-        elif(s_len==1):
+        elif s_len == 1:
             search(searcher[0])
         else:
             print("Sorry result not found")
-    
+
     else:
-        if(s_len>1):
+        if s_len > 1:
             for i in range(s_len):
-                print(f'{i+1}: {searcher[i]}')
+                print(f"{i+1}: {searcher[i]}")
 
-            f_term = int(input(f'select between 1 & {s_len}: ')) - 1
-            full_search(searcher[f_term])
+            print('0: Exit Search')
+            f_term = int(input(f"select between 1 & {s_len}: ")) - 1
+            if f_term+1>=1 and f_term+1<=s_len:
+                full_search(searcher[f_term])
+            else:
+                print('Exitting search')
 
-        elif(s_len==1):
+        elif s_len == 1:
             full_search(searcher[0])
         else:
             print("Sorry result not found")
 
 
-
-
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("-s","--search", type=str ,help="pass term for searching wikipedia")
-    argparser.add_argument("-F","--full", action='store_true', help="output all the pagedata from wikipedia")
-    argparser.add_argument("-i","--interactive",  action='store_true', help="Run the tool interactively(CTRL+C to quit)")
+    argparser.add_argument(
+        "-s", "--search", type=str, help="pass term for searching wikipedia"
+    )
+    argparser.add_argument(
+        "-F",
+        "--full",
+        action="store_true",
+        help="output all the pagedata from wikipedia",
+    )
+    argparser.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="Run the tool interactively(CTRL+C to quit)",
+    )
     args = argparser.parse_args()
-    
-    if(args.search):
-        term_search(args.search)
+
+    if args.full and args.search:
+        term_search(args.search, True)
+    elif args.search:
+        term_search(args.search, False)
     else:
-        while(True):
+        while True:
             search_term = input("What do you want to search : ")
             term_search(search_term)
